9670ed8e7e94bec5736013d722774d0e0221af6c
GrimeildineRF <grimeildine.rf@gmail.com>
2021-09-18 19:56:16 +0700

    Latihan Membuat Biodata diri

---
+++ Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo"Nama saya Grimeildine Risti Farrah
+			 saya tinggal di daerah Ciomas 
+			 Olahraga yang saya sukai adalah 
+			 Badminton, Berenang, dan Volley";
+	}
+}
