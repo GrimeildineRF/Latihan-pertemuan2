4f1003f61a000753411c5827f4ce55bdc64dfe9f
GrimeildineRF <grimeildine.rf@gmail.com>
2021-09-25 17:10:14 +0700

    penggunaan controllers dan input matakuliah

---
+++ Latihan1.php
@@ -0,0 +1,18 @@
+<?php
+class Latihan1 extends CI_Controller
+{
+public function index()
+	{
+		echo "<b><h2>SELAMAT DATANG MAHASISWA/MAHASISWI... SELMAT BERGABUNG DI KELAS WEB PROGRAMMING II...</h2><b>";
+	}
+
+	public function penjumlahan($n1, $n2)
+	{
+		$this->load->model('Model_latihan1');
+		$hasil = $this->Model_latihan1->jumlah($n1, $n2);
+		echo "Hasil Penjumlahan dari " . $n1 . " + " . $n2 . " = " . $hasil;
+		
+	}
+
+}
+
